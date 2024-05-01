11e596a0bf842cfaddac5f9ac3c4093ede29c207
LBTtirstocc <158111213+YubotKwng@users.noreply.github.com>
2024-05-01 16:18:22 +1000

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,25 +1,15 @@
 import pandas as pd
 
-# Define file names
-#ratings_file = 'BX-Ratings.csv'
-# Using cleadned books instead
-#books_file = 'cleaned_Books.csv'
-#users_file = 'BX-Users.csv'
-
-# Read data from CSV files
-#ratings_df = pd.read_csv(ratings_file)
-#books_df = pd.read_csv(books_file)
-
-def merge_rating(ratings_df, books_df):
+def merge_rating_book(ratings_df, books_df):
     # First merge: Ratings with Books on 'ISBN'
     ratings_books_merged = pd.merge(ratings_df, books_df, on='ISBN', how='inner')
 
     # Save the merged DataFrame to a new CSV file
     ratings_books_merged.to_csv('rating_withbooks.csv', index=False)
 
-merge_rating(ratings_df, books_df)
+merge_rating_book(ratings_df, books_df)
 
-def merge_users(ratings_df, books_df, users_df):
+def merge_all(ratings_df, books_df, users_df):
     # First merge: Ratings with Books on 'ISBN'
     ratings_books_merged = pd.merge(ratings_df, books_df, on='ISBN', how='inner')
 
@@ -29,5 +19,5 @@ def merge_users(ratings_df, books_df, users_df):
     # Save the merged DataFrame to a new CSV file
     final_merged_df.to_csv('final_merge.csv', index=False)
 
-merge_users(ratings_df, books_df, users_df)
+merge_all(ratings_df, books_df, users_df)
 
